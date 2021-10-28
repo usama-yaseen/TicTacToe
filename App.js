0872140350706e84fcdd89b4d5c33efa58a64c9c
usama-yaseen <usamayaseen07@gmail.com>
2021-10-29 00:44:32 +0500

    Working Board( Including Win Check)

--- App.js
+++ App.js
@@ -14,25 +14,97 @@ const NumButton = (props) => {
     <View >
       <TouchableOpacity
         onPress={() => {
+
+          //To Add A 'O' or 'X'
           props.Field.changeField(props.index, props.Player.getPlayer)
+          //To Change Turn
           props.Player.changePlayer(props.Player.getPlayer);
+          //To Check For A Win
+          var checkWin = props.Field.getField;
+
+          if ((checkWin[0] == checkWin[1]) && (checkWin[1] == checkWin[2]) && checkWin[0] != "") {
+            if (checkWin[0] == "O") {
+              console.log('Player 1 Wins! From 1');
+            }
+            else {
+              console.log("Player 2 Wins! From 1")
+            }
+          }
+          else if ((checkWin[2] == checkWin[4]) && (checkWin[2] == checkWin[6]) && checkWin[2] != "") {
+            if (checkWin[0] == "O") {
+              console.log('Player 1 Wins! From 2');
+            }
+            else {
+              console.log("Player 2 Wins! From 2")
+            }
+          }
+          else if ((checkWin[0] == checkWin[3]) && (checkWin[0] == checkWin[6]) && checkWin[0] != "") {
+            if (checkWin[0] == "O") {
+              console.log('Player 1 Wins! From 3');
+            }
+            else {
+              console.log("Player 2 Wins! From 3")
+            }
+          }
+          else if ((checkWin[0] == checkWin[4]) && (checkWin[0] == checkWin[8]) && checkWin[0] != "") {
+
+            if (checkWin[0] == "O") {
+              console.log('Player 1 Wins! From 4');
+            }
+            else {
+              console.log("Player 2 Wins! From 4")
+            }
+          }
+          else if ((checkWin[1] == checkWin[4]) && (checkWin[1] == checkWin[7]) && checkWin[1] != "") {
+            if (checkWin[1] == "O") {
+              console.log('Player 1 Wins! From 5');
+            }
+            else {
+              console.log("Player 2 Wins! From 5")
+            }
+          }
+          else if ((checkWin[2] == checkWin[5]) && (checkWin[2] == checkWin[8]) && checkWin[2] != "") {
+            if (checkWin[2] == "O") {
+              console.log('Player 1 Wins! From 6');
+            }
+            else {
+              console.log("Player 2 Wins! From 6")
+            }
+          }
+          else if ((checkWin[3] == checkWin[4]) && (checkWin[3] == checkWin[5]) && checkWin[3] != "") {
+            if (checkWin[3] == "O") {
+              console.log('Player 1 Wins! From 7');
+            }
+            else {
+              console.log("Player 2 Wins! From 7")
+            }
+          }
+          else if ((checkWin[6] == checkWin[7]) && (checkWin[6] == checkWin[8]) && checkWin[6] != "") {
+            if (checkWin[6] == "O") {
+              console.log('Player 1 Wins! From 8');
+            }
+            else {
+              console.log("Player 2 Wins! From 8")
+            }
+          }
+          else {
+            console.log("No One Has Won Yet! Please Continue")
+          }
+
         }}
         activeOpacity={0.5} style={styles.touchButton}>
-        <Text style={styles.text}>{props.Field.geField[props.index]}</Text>
-      </TouchableOpacity>s
+        <Text style={styles.text}>{props.Field.getField[props.index]}</Text>
+      </TouchableOpacity>
     </View>
   );
-
 };
 
 export default function App() {
-  const [getField, setField] = React.useState(["1", "2", "3", "4", "5", "6", "7", "8", "9",]);
+  const [getField, setField] = React.useState(["", "", "", "", "", "", "", "", "",]);
   const changeField = (x, y) => {
     let tempField = getField;
     tempField[x] = y;
-    console.log(tempField)
     setField(tempField);
-    console.log(getField)
   }
   const [getPlayer, setPlayer] = React.useState("O");
   const changPlayer = (x) => {
@@ -45,20 +117,20 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.ROW}>
-        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={0} />
-        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={1} />
-        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={2} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ getField: getField, changeField: changeField }} index={0} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ getField: getField, changeField: changeField }} index={1} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ getField: getField, changeField: changeField }} index={2} />
       </View>
       <View style={styles.ROW}>
-        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={3} />
-        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={4} />
-        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={5} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ getField: getField, changeField: changeField }} index={3} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ getField: getField, changeField: changeField }} index={4} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ getField: getField, changeField: changeField }} index={5} />
       </View>
       <View style={styles.ROW}>
-        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={6} />
-        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={7} />
-        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={8} />
-      </View> 
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ getField: getField, changeField: changeField }} index={6} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ getField: getField, changeField: changeField }} index={7} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ getField: getField, changeField: changeField }} index={8} />
+      </View>
     </View>
   );
 }
