6820d7fd767820ed8287e2127ee91659d63d53e5
usama-yaseen <usamayaseen07@gmail.com>
2021-10-28 22:14:51 +0500

    GUI

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  NativeModules,
+  View,
+  TouchableOpacity,
+} from "react-native";
+const { StatusBarManager } = NativeModules;
+
+const NumButton = (props) => {
+  // console.log(typeof props.index);
+  return (
+    <View >
+      <TouchableOpacity
+        onPress={() => {
+          props.Field.changeField(props.index, props.Player.getPlayer)
+          props.Player.changePlayer(props.Player.getPlayer);
+        }}
+        activeOpacity={0.5} style={styles.touchButton}>
+        <Text style={styles.text}>{props.Field.geField[props.index]}</Text>
+      </TouchableOpacity>s
+    </View>
+  );
+
+};
 
 export default function App() {
+  const [getField, setField] = React.useState(["1", "2", "3", "4", "5", "6", "7", "8", "9",]);
+  const changeField = (x, y) => {
+    let tempField = getField;
+    tempField[x] = y;
+    console.log(tempField)
+    setField(tempField);
+    console.log(getField)
+  }
+  const [getPlayer, setPlayer] = React.useState("O");
+  const changPlayer = (x) => {
+    if (x == "O")
+      setPlayer("X");
+    else {
+      setPlayer("O")
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.ROW}>
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={0} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={1} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={2} />
+      </View>
+      <View style={styles.ROW}>
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={3} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={4} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={5} />
+      </View>
+      <View style={styles.ROW}>
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={6} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={7} />
+        <NumButton Player={{ getPlayer: getPlayer, changePlayer: changPlayer }} Field={{ geField: getPlayer, changeField: changeField }} index={8} />
+      </View> 
     </View>
   );
 }
@@ -14,8 +66,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: 'blue',
+    marginTop: StatusBarManager.HEIGHT,
+  },
+  touchButton: {
+    width: 70,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    backgroundColor: 'red',
+  },
+  ROW: {
+    justifyContent: 'center',
+    flexDirection: "row",
+    width: "100%",
+  },
+  text: {
+    fontSize: 40,
   },
 });
